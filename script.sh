7b8de343819f6fbcd7e89c01aa49a413d3ac2125
sam <sam.max@ejemplo.com>
2025-01-18 20:58:09 +0100

    añade funcionalidad para saludar al usuario

--- script.sh
+++ script.sh
@@ -2,12 +2,17 @@
 echo "¡Hola! Este es un script para aprender Git desde cero."
 echo "1) Mostrar archivos en el directorio actual"
 echo "2) Mostrar fecha y hora"
-echo "3) Salir"
+echo "3) Saludar al usuario"
+echo "4) Salir"
 read -p "Selecciona una opción: " opcion
 
 case $opcion in
     1) ls ;;
     2) date ;;
-    3) echo "Adiós"; exit ;;
+    3)
+        read -p "Introduce tu nombre: " nombre
+        echo "¡Hola, $nombre! Espero que disfrutes aprendiendo Git."
+        ;;
+    4) echo "Adiós"; exit ;;
     *) echo "Opción no válida";;
 esac
